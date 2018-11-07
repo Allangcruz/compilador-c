35427f25dad3fbcffcd8caa80b79dd81e5284330
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2018-11-07 00:12:46 -0200

    Primeiro commit.

---
+++ compilador.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+#include <locale.h>
+
+// Alunos:
+// Nome 1
+// Nome 2
+
+// Armazena o total de memoria utilizada no compilador.
+int TOTAL_CONSUMO_MEMORIA = 0;
+
+// Valor maximo de cosumo de memoria em bytes 367001600 bytes => 350 megabytes
+int MAX_TOTAL_CONSUMO_MEMORIA = 367001600;
+
+int main () {
+	setlocale(LC_ALL, "PORTUGUESE");
+	printf("Compilador 2018");
+	
+	// Leitura de arquivo
+	
+	// Tabela de simbolos
+	
+	// Tratamento de erros
+	
+	// Gerenciamento de limite de memoria
+	
+    printf("\n\n");
+    system("pause");
+    
+    return 0;
+}
+
